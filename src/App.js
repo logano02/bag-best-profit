374365e7308fa448e26658ca2b8fafafd99fa479
Monthon Wannaphop <monthon_wan@AIA-MC-J8717.local>
2023-10-04 01:20:41 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField, Button, Container, Box } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
 
 function App() {
+  const [inputData, setInputData] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputData(e.target.value);
+  };
+
+  const generateResults = () => {
+    const lines = inputData.split("\n");
+    const regex = /(\d+)\s*กำไร\s*(\d+)/;
+
+    const newResults = lines
+      .map((line, index) => {
+        const match = line.match(regex);
+        if (match) {
+          const beforeProfit = parseFloat(match[1]);
+          const afterProfit = parseFloat(match[2]);
+          const percentage = ((afterProfit / beforeProfit) * 100).toFixed(2);
+          return {
+            key: index,
+            text: (
+              <span>
+                {line} : = <strong> {percentage}</strong> %
+              </span>
+            ),
+            value: percentage,
+            line: line,
+          };
+        }
+        return null; // Return null for lines that don't match the pattern
+      })
+      .filter((result) => result !== null);
+
+    setResults(newResults.sort((a, b) => b.value - a.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box display={"flex"}>
+        <h2>Best-Profit</h2>
+      </Box>
+      <Box mb={1}>
+        <TextField
+          label="Enter Text"
+          multiline
+          rows={6}
+          variant="outlined"
+          fullWidth
+          value={inputData}
+          onChange={handleInputChange}
+        />
+      </Box>
+      <Button variant="contained" color="primary" onClick={generateResults}>
+        Generate Best Profit!!
+      </Button>
+      <div>
+        {results.length > 0 && <h2>Generated Results:</h2>}
+        {results.map((result, index) => (
+          <Box
+            mb={1}
+            sx={{
+              color:
+                index === 0 ? "green" : index === 1 ? "dodgerblue" : "auto",
+            }}
+            display={"flex"}
+            key={result.key}
+          >
+            <Box mr={1}>{result.text}</Box>
+            <Box>
+              <IconButton aria-label="delete" size="small">
+                <CopyAllIcon
+                  fontSize="inherit"
+                  color="primary"
+                  onClick={() => {
+                    navigator.clipboard.writeText(result.line);
+                  }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+        ))}
+      </div>
+    </Container>
   );
 }
 
