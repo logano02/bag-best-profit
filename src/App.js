5cdcde586f180e46fa1f2eab38a5f29596fca8c4
Monthon Wannaphop <monthon_wan@AIA-MC-J8717.local>
2023-10-05 15:41:12 +0700

    add new logic check profit/date

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import IconButton from "@mui/material/IconButton";
 import CopyAllIcon from "@mui/icons-material/CopyAll";
 import "./App.css"; // Import your CSS file
 
+import moment from "moment";
+
 function App() {
   const [inputData, setInputData] = useState("");
   const [results, setResults] = useState([]);
@@ -12,14 +14,42 @@ function App() {
     setInputData(e.target.value);
   };
 
+  const calculateDateDifference = (matchDate) => {
+    const [day, month] = matchDate.split("/");
+    const currentDate = moment();
+    const currentMonth = currentDate.month() + 1;
+    const parsedDate = moment(
+      `${month.length === 1 ? "0" + month : month}-${
+        day.length === 1 ? "0" + day : day
+      }`,
+      "MM-DD"
+    );
+
+    let yearDifference = 0;
+    if (parsedDate.month() + 1 < currentMonth) {
+      yearDifference = 1;
+    }
+
+    parsedDate.add(yearDifference, "year");
+    const dayDifference = parsedDate.diff(currentDate, "days") + 1;
+
+    return dayDifference;
+  };
+
   const generateResults = () => {
     const lines = inputData.split("\n");
-    const regex = /(\d+)\s*กำไร\s*(\d+)/;
+    const regexProfit = /(\d+)\s*กำไร\s*(\d+)/;
+    const regexDate = /\b\d{1,2}\/\d{1,2}\b/;
 
     const newResults = lines
       .map((line, index) => {
-        const match = line.match(regex);
+        const match = line.match(regexProfit);
         if (match) {
+          const matchDate = line.match(regexDate);
+          let dayDifference = 1;
+          if (matchDate) {
+            dayDifference = calculateDateDifference(matchDate[0]);
+          }
           const beforeProfit = parseFloat(match[1]);
           const afterProfit = parseFloat(match[2]);
           const percentage = ((afterProfit / beforeProfit) * 100).toFixed(2);
@@ -27,18 +57,28 @@ function App() {
             key: index,
             text: (
               <span>
-                {line} : = <strong> {percentage}</strong> %
+                {line} = <strong> {percentage}</strong> %
+                {matchDate && (
+                  <>
+                    {` | กำไร/วัน = ${(
+                      10000 /
+                      percentage /
+                      dayDifference
+                    ).toFixed(0)}฿/${dayDifference}วัน`}{" "}
+                  </>
+                )}
               </span>
             ),
             value: percentage,
             line: line,
+            profit: (10000 / percentage / dayDifference).toFixed(0),
           };
         }
-        return null; // Return null for lines that don't match the pattern
+        return null;
       })
       .filter((result) => result !== null);
 
-    setResults(newResults.sort((a, b) => b.value - a.value));
+    setResults(newResults.sort((a, b) => b.profit - a.profit));
   };
 
   return (
@@ -64,6 +104,7 @@ function App() {
           </Button>
           <div>
             {results.length > 0 && <h3>Generated Results</h3>}
+            {results.length > 0 && <h4>เงินตั้งต้น คำนวน/ต่อวัน 10,000฿</h4>}
             {results.map((result, index) => (
               <Box
                 mb={1}
